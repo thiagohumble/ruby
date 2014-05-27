cda61230fc9d116fac7af377f95e7b331b8efa4e
thiagohumble <thg.humble@gmail.com>
2014-05-27 02:53:34 -0300

    Create filter_parameter_logging.rb

---
+++ colchonet/config/initializers/filter_parameter_logging.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+
+
+# Configure sensitive parameters which will be filtered 
+
+# from the log file.
+
+Rails.application.config.filter_parameters += [ 
+
+	:password,
+
+	:password_confirmation
+
+]
+
